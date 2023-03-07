35945884482c668f5dc16dba7b79b3817b70097e
Admin Server <apostlma@ucsd.edu>
2023-03-07 13:59:04 +0000

    added db connection  & made first query to db using cars/all

--- routes/cars.js
+++ routes/cars.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const db = require('../db')
+
 var carRouter = express.Router()
 carRouter.use(express.json())
 
 
 carRouter.get('/all', (req,res) => {
-    res.status(200).json({"all":"cars"})
+    const queryStatement  = 'SELECT * FROM cars;'
+    db.query(queryStatement, (error, results) => {
+        if (error) {
+            console.log(error)
+          res.status(500).json(error)  
+        } else {
+            console.log(results.rows)
+            res.status(200).json(results.rows)
+        }
+    }) 
 })
 
 
