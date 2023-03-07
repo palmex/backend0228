1a79ef75806d40294aa5cc65a699db6e539e91eb
Admin Server <apostlma@ucsd.edu>
2023-03-07 13:09:37 +0000

    added echo endpoint

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/first', (req,res) => {
     res.status(200).json({"success": "true"})
 })
 
+app.post('/echo',(req,res)=> {
+    console.log('headers: ', req.headers)
+    console.log('params: ', req.params)
+    console.log('body: ', req.body)
+    res.status(200).json(req.body)
+})
+
 app.get('/second', (req,res) => {
     console.log("Your second endpoint!")
     res.status(200).json({"success": "true"})
