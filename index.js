66b63d3fb8afc608262527d380425a1200b33e0d
Admin Server <apostlma@ucsd.edu>
2023-03-28 13:57:07 +0000

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 const userRouter = require('./routes/Users')
 
 app.use(express.json())
+app.use(cors())
 app.use('/users', userRouter)
 
 const carRouter = require('./routes/cars')
